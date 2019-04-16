940a7ec92cf65dc597dd79e955bb1f921d0056de
evstarostin89 <evstarostin89@yandex-team.ru>
2019-04-16 16:31:24 +0300

    Updates

---
+++ precache-manifest.771fe6a7ac7b192b97add63a3822a5f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fed282ca2b32de3a3be4",
+    "url": "/drawer/static/js/runtime~main.ac58c41b.js"
+  },
+  {
+    "revision": "995f76b85d5c01b9b47a",
+    "url": "/drawer/static/js/main.96b1b310.chunk.js"
+  },
+  {
+    "revision": "a340805f899c6f90c704",
+    "url": "/drawer/static/js/2.53812eed.chunk.js"
+  },
+  {
+    "revision": "995f76b85d5c01b9b47a",
+    "url": "/drawer/static/css/main.2a506e44.chunk.css"
+  },
+  {
+    "revision": "9fc5151daa9a9829ba4d299640e103d7",
+    "url": "/drawer/index.html"
+  }
+];
